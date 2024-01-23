1c585f5548c50768b01126b5e43b0cc72671f50b
NuiSupachai <44989218+NuiSupachai@users.noreply.github.com>
2024-01-23 23:23:33 +0700

    se d:; cd 'd:\Python Script\Morning Check list BAY'; & 'C:\Users\006640_Admin\AppData\Local\Programs\Python\Python310\python.exe' 'c:\Users\006640_Admin\.vscode\extensions\ms-python.python-2023.22.1\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '58365' '--' 'd:\Python Script\Morning Check list BAY\template.py'

--- template.py
+++ template.py
@@ -1,7 +1,5 @@
 import xlsxwriter
 workbook = xlsxwriter.Workbook('Morning Check List.xlsx')
-sheet_TotalAP = workbook.add_worksheet('AP Status')
-
 
 # Define cell formats for header
 header_format = workbook.add_format(
@@ -22,7 +20,8 @@ data_format = workbook.add_format(
 )
 
 # Create a new Excel workbook and add a worksheet
-def AP_Status():
+def APStatus():
+    sheet_TotalAP = workbook.add_worksheet('AP Status')
     sheet_TotalAP.merge_range('A1:F1','AP STAUS',header_format)
     sheet_TotalAP.merge_range('A2:B2','KSRO',header_format)
     sheet_TotalAP.merge_range('C2:D2','KSRM',header_format)
@@ -35,7 +34,22 @@ def AP_Status():
     sheet_TotalAP.write('E3', 'Up', header_format)
     sheet_TotalAP.write('F3', 'Down', header_format)
 
-    # Save the workbook
-    workbook.close()
+def CheckSSID_KSRO():
+    sheet_TotalAP = workbook.add_worksheet('KSRO')
+    sheet_TotalAP.merge_range('A1:A2','Floor',header_format)
+    sheet_TotalAP.merge_range('B:K1','BSSID STATUS',header_format)
 
-AP_Status()
+    # Populate the template data
+    sheet_TotalAP.write('B2', '@Enterprise', header_format)
+    sheet_TotalAP.write('C2', 'Narita', header_format)
+    sheet_TotalAP.write('D2', 'Challenger', header_format)
+    sheet_TotalAP.write('E2', 'Gesuto', header_format)
+    sheet_TotalAP.write('F2', 'Odaiba', header_format)
+    sheet_TotalAP.write('G2', 'Akiba', header_format)
+    sheet_TotalAP.write('H2', 'Atlantis', header_format)
+    sheet_TotalAP.write('I2', 'TABASCO', header_format)
+    sheet_TotalAP.write('J2', '@Telephone', header_format)
+    sheet_TotalAP.write('K2', 'WASABI', header_format)
+
+# Save the workbook
+workbook.close()

5d6ce3b9ed91a9dfb6a7e3af7918a94191dd7433
NuiSupachai <44989218+NuiSupachai@users.noreply.github.com>
2024-01-24 16:08:40 +0700

    edit main.py increased for loop.

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ from template import *
 
 mm_ip = '172.20.20.100'
 
+ksro_cluster =[]
 ksrm_cluster =[]
+kspo_cluster =[]
+
 ap_db = ssh_mc('show ap database status up | include Up | exclude Uplink', mm_ip)
 ap_db = ap_db.splitlines()
-print("Command Output:\n", ap_db)
-count = 0
+
+
 for i in ap_db:
-    count =+1
-    arr_apdb = i.split()
-    print("Command Output:\n", arr_apdb[6])
-    if arr_apdb[6] =='172.20.20.9':
-        ap_ksrm = arr_apdb[6]
+    list_apdb = i.split()
+    print("Command Output:\n", list_apdb[7])
+    if list_apdb[7] =='172.28.254.2' or list_apdb[7] =='172.28.254.3':
+        ap_ksro = list_apdb[7]
+        ksro_cluster.append(ap_ksro)
+    elif list_apdb[7] =='172.29.0.2' or list_apdb[7] =='172.29.0.3':
+        ap_ksrm = list_apdb[7]
         ksrm_cluster.append(ap_ksrm)
-count = ksrm_cluster.count('172.20.20.9')
-print(count)
+    else:
+        ap_kspo = list_apdb[7]
+        kspo_cluster.append(ap_kspo)
+"""elif list_apdb[7] =='172.29.128.2' or list_apdb[7] =='172.29.128.2' or list_apdb[7] =='172.29.128.2':
+        ap_kspo = list_apdb[7]
+        kspo_cluster.append(ap_kspo)"""
+
+ap_count_ksro = ksro_cluster.count(ap_ksro)
+ap_count_ksrm = ksrm_cluster.count(ap_ksrm)
+ap_count_kspo = kspo_cluster.count(ap_kspo)
+
+print(ap_count_ksro)
+print(ap_count_ksrm)
+print(ap_count_kspo)
